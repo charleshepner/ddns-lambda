0164b15486914c7331aca9e54db3e5eb8c7eba8b
Charles Hepner <charleshepner@gmail.com>
2021-02-18 16:12:22 -0500

    rename vars to ddns and implement basic auth

--- handler.py
+++ handler.py
@@ -1,27 +1,50 @@
+import base64
+import boto3
 import json
+import logging
+import os
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+ddns_username = os.environ.get("DDNS_USERNAME")
+ddns_password = os.environ.get("DDNS_PASSWORD")
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+def ddns_lambda(event, context):
+    logger.debug(event)
+
+    allow_access = False
+    headers = event.get('headers')
+    if headers:
+        auth = headers.get("Authorization")
+        if auth:
+            allow_access = check_credentials(auth)
+
+    if not allow_access:
+        response = format_response(401, "Unauthorized")
+
+    if allow_access:
+        response = format_response(200, "Congrats! You're authorized.")
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
 
-def authorizerFunc(event, context):
-    pass
+def check_credentials(authorization_header):
+    try:
+        base64_authentication_string = authorization_header.replace("Basic ", "")
+        authentication_string = base64.b64decode(base64_authentication_string).decode("utf-8")
+        provided_username, provided_password = tuple(authentication_string.split(":"))
+        if provided_username == ddns_username and provided_password == ddns_password:
+            return True
+    except Exception as ex:
+        logger.error(repr(ex))
+        return False
+
+    return False
+
+def format_response(status, message):
+    return {
+        "statusCode": status,
+        "body": json.dumps({"status": status, "message": message })
+    }
+
